9257882f0981fcf590f900840067cd2521a93c3e
bertamatu <info.berta@gmail.com>
2020-07-10 23:59:55 +0300

    redux

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,15 @@
 import thunk from "redux-thunk";
 import { applyMiddleware, createStore } from "redux";
 import weatherReducer from "../reducers/index";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 //---middleware
 const middleware = applyMiddleware(thunk);
 
+const composeEnhancers = composeWithDevTools(middleware);
+
 //---store
-const store = createStore(weatherReducer, middleware);
+const store = createStore(weatherReducer, composeEnhancers);
 
 export default store;
 
