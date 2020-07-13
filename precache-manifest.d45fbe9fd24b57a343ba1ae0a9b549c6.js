ba80739855eb33a872b336b610758de460a07830
bertamatu <info.berta@gmail.com>
2020-07-13 22:22:57 +0300

    Updates

---
+++ precache-manifest.d45fbe9fd24b57a343ba1ae0a9b549c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c01d6a0043667547a25eb909e6d93021",
+    "url": "/bertamatu/weather-app-reactjs-hooks-redux/index.html"
+  },
+  {
+    "revision": "1809048665ebe1676fa0",
+    "url": "/bertamatu/weather-app-reactjs-hooks-redux/static/css/main.01d2c12d.chunk.css"
+  },
+  {
+    "revision": "21225ebfa8586a857bed",
+    "url": "/bertamatu/weather-app-reactjs-hooks-redux/static/js/2.6368fce2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bertamatu/weather-app-reactjs-hooks-redux/static/js/2.6368fce2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1809048665ebe1676fa0",
+    "url": "/bertamatu/weather-app-reactjs-hooks-redux/static/js/main.d9dc8034.chunk.js"
+  },
+  {
+    "revision": "81a1d51c26d48deabb1d",
+    "url": "/bertamatu/weather-app-reactjs-hooks-redux/static/js/runtime-main.724290a8.js"
+  }
+]);
